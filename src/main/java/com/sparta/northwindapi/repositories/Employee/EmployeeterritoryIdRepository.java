cdd3b4bda832008cc25c97d56d94b96c6ab3645f
HamzMas <32570820+HamzMas@users.noreply.github.com>
2022-11-10 14:36:23 +0000

    Delete IdRepos

--- src/main/java/com/sparta/northwindapi/repositories/Employee/EmployeeterritoryIdRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.northwindapi.repositories.Employee;
-
-import com.sparta.northwindapi.entities.Employee.EmployeeterritoryId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeterritoryIdRepository extends JpaRepository<EmployeeterritoryId, Integer> {
-}
